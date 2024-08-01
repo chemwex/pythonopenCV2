c458f1f22b17755b4c27cc861d9ec3f57214395e
Geoffrey Chemwa <wexche@gmail.com>
2024-08-01 11:58:59 -0500

    Add files via upload

---
+++ Project___DisplayCat___opencv_2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 12:30:40 2024
+
+@author: Geoffrey Chemwa
+"""
+
+import cv2
+
+#cv2.imread('cat.jfif',0)
+imgray = cv2.imread('lena.jpg',0) #read image color in grayscale.
+print(imgray)
+imgcolor = cv2.imread('lena.jpg',1) #read image file in color.
+print(imgcolor)
+imgasis = cv2.imread('lena.jpg',-1) #read image file as is.
+print(imgasis)
+
+cv2.imshow('imagegray',imgray)   #show the image in a window whose title is image
+cv2.imshow('imagecolor',imgcolor)   #show the image in a window whose title is image
+cv2.imshow('imageasis',imgasis)   #show the image in a window whose title is image   
+cv2.waitKey(5000)         #show the window for 5 seconds on the screen
+cv2.destroyAllWindows()   #destroy the created window
